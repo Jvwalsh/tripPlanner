95a442ab774af254d0a965b62e26c504c48afa49
Jvwalsh <jvwalsh@icloud.com>
2018-03-20 13:32:30 -0500

    testing

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const mapboxgl = require("mapbox-gl");
+const multiFunc = require('./marker');
+
+mapboxgl.accessToken = "pk.eyJ1Ijoicmlja3kwMzIxIiwiYSI6ImNqZXp1MmVsaDBmdWEyeG9oNWlueHR2b3gifQ.C2Ej5Uix1g5985Myju82Sg";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+
+let tempMark = multiFunc('activity', [-87.6254, 41.8685]);
+let tempMark2 = multiFunc('restaurant', [-87.6254, 41.5685]);
+// let tempMark2 = 
+
+// const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = tempMark;
+
+
+
+
+// new mapboxgl.Marker(document.getElementById(markerDomEl)).setLngLat([-87.6354, 41.8885]).addTo(map);
+// new mapboxgl.Marker(document.getElementById(markerDomEl)).setLngLat([-87.6454, 41.8985]).addTo(map);
+new mapboxgl.Marker(document.getElementById(tempMark.markerDomEl)).setLngLat(tempMark.location).addTo(map);
+
+new mapboxgl.Marker(document.getElementById(tempMark2.markerDomEl)).setLngLat(tempMark2.location).addTo(map);
+
+
---
+++ src/marker.js
@@ -0,0 +1,44 @@
+const mapboxgl = require("mapbox-gl");
+const oneFunction = function (string, location) {
+
+    mapboxgl.accessToken = "pk.eyJ1Ijoicmlja3kwMzIxIiwiYSI6ImNqZXp1MmVsaDBmdWEyeG9oNWlueHR2b3gifQ.C2Ej5Uix1g5985Myju82Sg";
+
+    // console.log('called it!');
+    // console.log(string);
+    // console.log(location);
+
+    //string is the type of marker
+
+    //location is the array coords
+    let bckGround;
+    
+
+    if (string === "hotel") {
+        bckGround = "url(http://i.imgur.com/D9574Cu.png)";
+
+    }
+    else if (string === 'activity') {
+        console.log('success');
+        bckGround = "url(http://i.imgur.com/WbMOfMl.png)";
+
+    }
+    else if (string === 'restaurant') {
+        bckGround = "url(http://i.imgur.com/cqR6pUI.png)";
+    }
+
+    // return {
+    //     bckGround, location};
+    
+    const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+    markerDomEl.style.width = "32px";
+    markerDomEl.style.height = "39px";
+    markerDomEl.style.backgroundImage =bckGround;
+    console.log(markerDomEl, "dom El");
+
+    // let markerNew = new mapboxgl.Marker(document.getElementById(markerDomEl)).setLngLat(location).addTo(map);
+    // console.log(markerNew, 'look here');
+    return {markerDomEl, location}
+}
+
+
+module.exports = oneFunction
